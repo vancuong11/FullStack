dd0c26711a1990650719a9bd6cf5fe0ee6aa326c
vancuong11 <cuongg121101@gmail.com>
2023-03-30 13:20:08 +0700

    Mo hinh MVC

---
+++ src/controllers/homeController.js
@@ -0,0 +1,7 @@
+let getHomePage = (req, res) => {
+    return res.render('homepage');
+};
+
+module.exports = {
+    getHomePage,
+};
--- src/routes/web.js
+++ src/routes/web.js
@@ -1,11 +1,13 @@
 import express from 'express';
-
+import homeController from '../controllers/homeController';
 let router = express.Router();
 
 let initWebRoute = (app) => {
     router.get('/', (req, res) => {
         res.send('Welcome Server Node');
     });
+
+    router.get('/home', homeController.getHomePage);
     return app.use('/', router);
 };
 
