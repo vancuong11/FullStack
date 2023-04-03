096a5dac80727882adf06561e912ddf0e5872823
vancuong11 <cuongg121101@gmail.com>
2023-04-03 12:25:17 +0700

    Tao table, them du lieu database voi Sequelize CLI

--- src/config/connectDB.js
+++ src/config/connectDB.js
@@ -2,7 +2,7 @@
 import Sequelize from 'sequelize';
 
 // Option 3: Passing parameters separately (other dialects)
-const sequelize = new Sequelize('nodejsbasic', 'root', null, {
+const sequelize = new Sequelize('bookingcare', 'root', null, {
     host: 'localhost',
     dialect: 'mysql',
     logging: false,
