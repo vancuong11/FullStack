62290c18e55dced2d1d3758f5f387319fc1168a9
vancuong11 <cuongg121101@gmail.com>
2023-04-10 15:02:42 +0700

    Fix CORS error

--- src/index.js
+++ src/index.js
@@ -1,12 +1,32 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 require('dotenv').config();
+// import cors from 'cors';
 
 import viewEngine from './config/viewEngine';
 import initWebRoute from './routes/web';
 import connectDB from './config/connectDB';
 
 let app = express();
+
+// Add headers before the routes are defined
+app.use(function (req, res, next) {
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+
+    // Pass to next layer of middleware
+    next();
+});
 
 // config app
 app.use(bodyParser.json());
