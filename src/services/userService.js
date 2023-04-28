5908ad2e741344712024f8e9ed2145f5bc9986ea
vancuong11 <cuongg121101@gmail.com>
2023-04-28 12:42:46 +0700

    change information user

--- src/services/userService.js
+++ src/services/userService.js
@@ -9,7 +9,7 @@ let handleUserLogin = (email, password) => {
             let isExits = await checkUserEmail(email);
             if (isExits) {
                 let user = await db.User.findOne({
-                    attributes: ['email', 'roleId', 'password'],
+                    attributes: ['email', 'roleId', 'password', 'firstName', 'lastName'],
                     where: { email: email },
                     raw: true,
                 });
@@ -103,8 +103,9 @@ let createNewUser = (data) => {
                     lastName: data.lastName,
                     address: data.address,
                     phoneNumber: data.phoneNumber,
-                    gender: data.gender === '1' ? true : false,
+                    gender: data.gender,
                     roleId: data.roleId,
+                    positionId: data.positionId,
                 });
                 resolve({
                     errCode: 0,
