1d82269b70cf149cd61349893c9d8d8ea50ecec0
vancuong11 <cuongg121101@gmail.com>
2023-05-04 10:04:20 +0700

    Write & Read Image

--- src/index.js
+++ src/index.js
@@ -28,9 +28,12 @@ app.use(function (req, res, next) {
     next();
 });
 
-// config app
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+// // config app
+// app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(bodyParser.json({ limit: '50mb' }));
+app.use(bodyParser.urlencoded({ extended: true, limit: '50mb' }));
 
 viewEngine(app);
 initWebRoute(app);
